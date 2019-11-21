6ef4ae02956e8eee043cdf66c0bd73fbc958bbc2
FantasticFiasco <mattias.kindborg@gmail.com>
2019-11-21 16:13:59 +0100

    chore: add serilog example

---
+++ serilog/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Bogus;
+using Serilog;
+using Serilog.Sinks.Http.BatchFormatters;
+using SerilogExample.Generators;
+
+namespace SerilogExample
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.DurableHttpUsingFileSizeRolledBuffers(
+                    requestUri: "http://fluentd:9880/myapp.test",
+                    batchFormatter: new ArrayBatchFormatter())
+                .WriteTo.Console()
+                .CreateLogger()
+                .ForContext<Program>();
+
+            var customerGenerator = new CustomerGenerator();
+            var orderGenerator = new OrderGenerator();
+
+            while (true)
+            {
+                var customer = customerGenerator.Generate();
+                var order = orderGenerator.Generate();
+
+                logger.Information("{@customer} placed {@order}", customer, order);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
